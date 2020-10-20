444d6e3c727f06a8b41e6787f899b7f4eb799317
yizuswall <jechual@gmail.com>
2020-10-20 12:57:21 -0400

    Cambios upgrade

---
+++ src/main/java/com/sap/yizuslabs/datafusion/DatafusionApplication.java
@@ -0,0 +1,13 @@
+package com.sap.yizuslabs.datafusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/yizuslabs/datafusion/DatafusionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sap.yizuslabs.datafusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatafusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hola");	
+	}
+
+}
