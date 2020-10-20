b3917646f91ccfbd8a15bd37da8ede4b6aa31384
Fabrizzio Silva <marco.pucp@gmail.com>
2020-10-20 15:27:15 -0500

    miau

--- src/main/java/com/sap/yizuslabs/datafusion/DatafusionApplication.java
+++ src/main/java/com/sap/yizuslabs/datafusion/DatafusionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DatafusionApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DatafusionApplication.class, args);
 	}
 
